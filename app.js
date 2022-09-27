102b59b6ee4b814be189f0185cda5ffa9a1745fc
Koray Kalkan <kalkankoray@gmail.com>
2022-09-27 23:56:49 +0300

    CleanBlog HW2 Done

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
 const path = require('path');
 app.use(express.static('public'));
 port = 3000;
-const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-};
 app.listen(port, () => {
     console.log(`Sunucu ${port} port da başlatildi.`);
 });
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public / index.html'));
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+app.get('/post', (req, res) => {
+    res.render('post');
 });
